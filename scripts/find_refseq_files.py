a9215aa5472e4ec19589ba23e6c8b69f9c7a8a1d
Yin <hsieh.y.chen@uit.no>
2021-06-01 16:15:17 +0200

    added fasta globbing script

---
+++ scripts/find_refseq_files.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+find_refseq_files.py
+
+Globs in a given directory for refseq files
+corresponding to given 4-letter PDB ID
+"""
+
+from pathlib import Path
+
+def iscorrect_pdbid(pdbid):
+    """Checks if pdbid is a 4-char string
+    Returns the lowercase pdbid"""
+    if not isinstance(pdbid, str):
+        raise TypeError('PDB ID must be str.')
+    elif not len(pdbid)==4:
+        raise Exception('PDB ID must have 4 characters.')
+    else:
+        return pdbid.lower()
+
+
+def refseq_formatter(pdbid):
+    """Returns formatted refseq fasta str expression
+
+    :param pdbid: str
+    
+    :returns: str with matching regex
+    """
+    return f'{pdbid}*refseq.fasta'
+
+
+def get_globbed_list(pathtodir, target):
+    """Searches directory for matches to target
+
+    :param pathtodir: pathlib.PosixPath 
+    :param target: str with expression to match
+
+    :returns: list of matching paths
+    """
+    p = pathtodir.expanduser()
+    return list(p.glob(target))
+    
