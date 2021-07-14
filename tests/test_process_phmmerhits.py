268ca7ba8b0505a2b0a01e33e2cd9ccf398f5fb6
Yin <hsieh.y.chen@uit.no>
2021-07-14 15:15:09 +0200

    added tests for getting hit list and orgset/dict

--- tests/test_process_phmmerhits.py
+++ tests/test_process_phmmerhits.py
@@ -8,7 +8,21 @@ import pytest
 
 sys.path.append("../scripts/")
 
-from process_phmmerhits import get_two_keyfiles, get_hit_list
+from process_phmmerhits import get_two_keyfiles, get_hit_list, get_orgs_from_hitlist
+from ordered_set import OrderedSet
+
+def test_get_two_keyfiles_globber():
+    testphmmerpath = Path('../testdata')
+    testpdbid = '1c0f'
+    res = get_two_keyfiles(testphmmerpath, testpdbid)
+    assert(res[0].name == '1c0f_A_refseq_phmmer.keyfile') 
+    assert(res[1].name == '1c0f_S_refseq_phmmer.keyfile') 
+
+def test_get_two_keyfiles_not_2_error():
+    testphmmerpath = Path('../testdata')
+    testpdbid = '1d4x'
+    with pytest.raises(ValueError):
+        get_two_keyfiles(testphmmerpath, testpdbid)
 
 
 def test_get_hit_list_res_max():
@@ -36,15 +50,19 @@ def test_get_hit_list_error_relation():
     with pytest.raises(ValueError):
         get_hit_list(testkeyfile, 1000, 'DEANRONALD')
 
-def test_get_two_keyfiles_globber():
-    testphmmerpath = Path('../testdata')
-    testpdbid = '1c0f'
-    res = get_two_keyfiles(testphmmerpath, testpdbid)
-    assert(res[0].name == '1c0f_A_refseq_phmmer.keyfile') 
-    assert(res[1].name == '1c0f_S_refseq_phmmer.keyfile') 
+def test_get_hit_list(rel='MINIMUM', thresh=0):
+    minikeyfile=Path('../testdata/1c0f_A_refseq_phmmer.testkeyfile')
+    res_hitlist = get_hit_list(minikeyfile, thresh, rel)
+    correct_hitlist = ['tr|_HUMAN', 'sp|_TOXCA', 'tr|_TOXCA', 'sp|_PANTR']
+    assert(res_hitlist == correct_hitlist)
 
-def test_get_two_keyfiles_not_2_error():
-    testphmmerpath = Path('../testdata')
-    testpdbid = '1d4x'
-    with pytest.raises(ValueError):
-        get_two_keyfiles(testphmmerpath, testpdbid)
+
+def test_get_orgs_from_hitlist():
+    hitlist = ['tr|_HUMAN', 'sp|_TOXCA', 'tr|_TOXCA', 'sp|_PANTR']
+    res_orgset, res_orgdict = get_orgs_from_hitlist(hitlist)
+    correct_orgset = OrderedSet(['HUMAN','TOXCA','PANTR'])
+    correct_orgdict = {'HUMAN':['tr|_HUMAN'],
+                       'TOXCA':['sp|TOXCA','tr|TOXCA'],
+                       'PANTR':['sp|PANTR']}
+    assert(res_orgset == correct_orgset)
+    assert(res_orgset == correct_orgdict)
