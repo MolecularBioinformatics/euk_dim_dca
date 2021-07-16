adf56d7329449f4163d14d8af09068b01364634c
Yin <hsieh.y.chen@uit.no>
2021-07-16 14:42:12 +0200

    added run phmmer tests

--- tests/test_run_phmmer.py
+++ tests/test_run_phmmer.py
@@ -14,7 +14,30 @@ from run_phmmer import *
 def test_phmmerlog_formatter():
     seqpath = Path('../testdata/ZZZZ_A_refseq.fasta')
     correctfmt = 'ZZZZ_A_refseq_phmmer.log'
-    assert(phmmerlog_formatter(seqpath) == correctfmt)
+    res = phmmerlog_formatter(seqpath)
+    assert(res == correctfmt)
 
 def test_run_phmmer_filenotfound():
-    pass
+    dbpath = Path()
+    seqpath = Path('../testdata/ZZZZ_A_refseq.fasta')
+    phmmerpath = Path('../testdata')
+    redo = False
+    with pytest.raises(FileNotFoundError):
+        run_phmmer(dbpath, seqpath, phmmerpath, redo)
+
+def test_run_phmmer_run_unsuccessful():
+    dbpath = Path()
+    seqpath = Path('../testdata/1c0f_A_refseq.fasta')
+    phmmerpath = Path('../testdata')
+    redo = True
+    with pytest.raises(ValueError):
+        run_phmmer(dbpath, seqpath, phmmerpath, redo)
+
+def test_run_phmmer_no_redo():
+    dbpath = Path()
+    seqpath = Path('../testdata/1c0f_A_refseq.fasta')
+    phmmerpath = Path('../testdata')
+    redo = False
+    correctoutpath = Path('../testdata/1c0f_A_refseq_phmmer.log')
+    res = run_phmmer(dbpath, seqpath, phmmerpath, redo)
+    assert(res == correctoutpath)
