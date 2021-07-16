60d5ccf89c49267e0ebfd269daefa9763c101f2f
Yin <hsieh.y.chen@uit.no>
2021-07-16 15:56:55 +0200

    fixed conditionals and error raising

--- scripts/run_easel_getseqs.py
+++ scripts/run_easel_getseqs.py
@@ -5,31 +5,23 @@ run_easel_getseqs.py
 Runs HMMER easel tool to extract sequences from database.
 
 Takes in a matched keyfile.
+
+Returns fasta file with extracted sequences.
 """
 
 import subprocess
 from pathlib import Path
 
-from io_utils import does_target_exist
-
-def easeled_seq_formatter(keyfilepath):
-    """Returns format of FASTA file with seqs
-    extracted from easel
-
-    :param keyfilepath: pathlib.PosixPath to keyfile
-    :returns: str, outfile name
-    """
-    return f'{keyfilepath.stem}.fasta'
-
+from io_utils import does_target_exist, easeled_seq_formatter
 
 def run_easel(easelpath, databasepath, phmmerpath, keyfilepath, redo):
     """
     Spawns subprocess to run esl-sfetch.
 
-    :param easelpath:
-    :param databasepath:
-    :param keyfilepath:
-    :param redo:
+    :param easelpath: pathlib.PosixPath
+    :param databasepath: pathlib.PosixPath
+    :param keyfilepath: pathlib.PosixPath
+    :param redo: bool, whether to re-extract
     """
     filename = easeled_seq_formatter(keyfilepath)
     outpath = phmmerpath.joinpath(filename)
@@ -39,17 +31,17 @@ def run_easel(easelpath, databasepath, phmmerpath, keyfilepath, redo):
     elif does_target_exist(outpath, 'file') and redo == False:
         print(f'Easel-fetched Fasta file: ({outpath}) already exists in {outpath.parent}')
         return outpath
-    else:
-        try:
-            f = open(outpath, 'w+')
-            cmdargs = [f"{easelpath}/esl-sfetch",
-                       "-f",
-                       f"{databasepath}",
-                       f"{keyfilepath}"] 
-            proc = subprocess.run(cmdargs, stdout=f)
-            f.close()
-            if proc.returncode == 0:
-                print(f'Retrieved seqs in fasta file: {outpath}')
-            return outpath
-        except:
-            raise Exception('Could not extract seqs with easel')
+
+    f = open(outpath, 'w+')
+    cmdargs = [f"{easelpath}/esl-sfetch",
+                "-f",
+               f"{databasepath}",
+               f"{keyfilepath}"] 
+    proc = subprocess.run(cmdargs, stdout=f)
+    f.close()
+
+    if proc.returncode != 0:
+        return ValueError('Easel extract unsuccessful!')
+
+    print(f'Retrieved seqs in fasta file: {outpath}')
+    return outpath
