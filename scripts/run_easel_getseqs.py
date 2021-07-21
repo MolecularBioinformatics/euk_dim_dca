4cba94643f575910e228bcf3f749f81da43adfef
Yin <hsieh.y.chen@uit.no>
2021-07-21 15:19:08 +0200

    run easel iteratively

--- scripts/run_easel_getseqs.py
+++ scripts/run_easel_getseqs.py
@@ -22,6 +22,8 @@ def run_easel(easelpath, databasepath, phmmerpath, keyfilepath, redo):
     :param databasepath: pathlib.PosixPath
     :param keyfilepath: pathlib.PosixPath
     :param redo: bool, whether to re-extract
+
+    :returns outpath: pathlib.PosixPath, path to fasta with extracted seqs
     """
     filename = easeled_seq_formatter(keyfilepath)
     outpath = phmmerpath.joinpath(filename)
@@ -45,3 +47,36 @@ def run_easel(easelpath, databasepath, phmmerpath, keyfilepath, redo):
 
     print(f'Retrieved seqs in fasta file: {outpath}')
     return outpath
+
+
+def run_easel_iterate(easelpath, databasepath, phmmerpath, keyfilepath, redo):
+    """Easel stops if it cannot find a sequence, no way
+    to get it to continue sequence extraction.
+    
+    This function iterates over all seq accession ids,
+    extracting one sequence at a time
+
+    :param easelpath: pathlib.PosixPath
+    :param databasepath: pathlib.PosixPath
+    :param keyfilepath: pathlib.PosixPath
+    :param redo: bool, whether to re-extract
+    """
+    filename = easeled_seq_formatter(keyfilepath)
+    outpath = phmmerpath.joinpath(filename)
+
+    if not does_target_exist(keyfilepath, 'file'):
+        raise FileNotFoundError(f'KEYFILE MISSING: Could not find {keyfilepath}!')
+    elif does_target_exist(outpath, 'file') and redo == False:
+        print(f'Easel-fetched Fasta file: ({outpath}) already exists in {outpath.parent}')
+        return outpath
+
+    # get list of ids
+
+    for item in idlist:
+        cmd = [f'{easelpath}/esl-sfetch',
+               f'{databasepath}',
+               f'{item}']
+        proc = subprocess.run(cmd, stdout=subprocess.PIPE)
+        if proc.returncode != 0:
+            print('bla')  # issue some warning
+        seq = proc.stdout.decode("utf-8")
