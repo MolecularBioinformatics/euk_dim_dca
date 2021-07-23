5f8c22c2eadb6f5412391e82ff22473d0247d61e
Yin <hsieh.y.chen@uit.no>
2021-07-23 14:17:22 +0200

    redo option incorporated

--- scripts/align_seqs.py
+++ scripts/align_seqs.py
@@ -27,7 +27,7 @@ def add_refseq(fastafile_path, refseqfile_path):
     writeout_fasta(fastafile_path, fadict, overwrite=True, addict=refseqdict)
 
 
-def run_muscle(fastafile_path, refseqfile_path, phmmerpath):
+def run_muscle(fastafile_path, refseqfile_path, phmmerpath, redo):
     """
     Spawns subprocess to run muscle.
 
@@ -36,14 +36,17 @@ def run_muscle(fastafile_path, refseqfile_path, phmmerpath):
 
     :returns alnfile_path: pathlib.PosixPath, aligned fasta
     """
+    outpath = phmmerpath / Path(f'{fastafile_path.stem}.aln')
+
     if not does_target_exist(fastafile_path, 'file'):
         raise FileNotFoundError(f'Fasta file with seqs to align not found: {fastafile_path}.')
     elif fastafile_path.stat().st_size == 0:
         raise ValueError(f'EMPTY FILE: {fastafile_path}.')
+    elif does_target_exist(outpath, 'file') and redo==False:
+        print(f'Alignment file {outpath} already exists. Give --redo True to realign.')
 
     add_refseq(fastafile_path, refseqfile_path)
     
-    outpath = phmmerpath / Path(f'{fastafile_path.stem}.aln')
     cmdargs = ["muscle", 
                "-in",
                f"{fastafile_path}",
