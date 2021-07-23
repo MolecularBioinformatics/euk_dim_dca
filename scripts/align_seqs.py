c363f02d256ec789f80a6615596ccbf1ec1d9cca
Yin <hsieh.y.chen@uit.no>
2021-07-23 13:56:11 +0200

    sequence alignment with muscle script

---
+++ scripts/align_seqs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+align_seqs.py
+
+Runs muscle alignment for a given set of sequences.
+
+Appends refseq back into seq file before alignment.
+"""
+
+import time
+import subprocess
+from pathlib import Path
+
+from io_utils import does_target_exist 
+
+def run_muscle(fastafile_path, phmmerpath):
+    """
+    Spawns subprocess to run muscle.
+
+    :param fastafile_path: pathlib.PosixPath
+    :param phmmerpath: pathlib.PosixPath
+
+    :returns alnfile_path: pathlib.PosixPath, aligned fasta
+    """
+    if not does_target_exist(fastafile_path, 'file'):
+        raise FileNotFoundError(f'Fasta file with seqs to align not found: {fastafile_path}.')
+    elif fastafile_path.stat().st_size == 0:
+        raise ValueError(f'EMPTY FILE: {fastafile_path}.')
+    
+    outpath = phmmerpath / Path(f'{fastafile_path.stem}.aln')
+    cmdargs = ["muscle", 
+               "-in",
+               f"{fastafile_path}",
+               "-out",
+               f"{outpath}"]
+
+    start = time.perf_counter()
+    proc = subprocess.run(cmdargs)
+    stop = time.perf_counter()
+    if proc.returncode != 0:
+        raise ValueError(f'Muscle could not align {fastafile_path}.')
+    print(f'Muscle ran in {stop-start:0.4f} seconds')
+    print(f'Alignment stored in {outpath}')
+    return outpath
