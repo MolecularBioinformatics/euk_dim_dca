91f113b45e54965ccddce40f364183014a60e6c2
Yin <hsieh.y.chen@uit.no>
2021-07-26 15:29:02 +0200

    fixed file appending error, switched to overwriting with redo = True

--- scripts/process_alnseqs.py
+++ scripts/process_alnseqs.py
@@ -76,7 +76,7 @@ def process_alnseqs(alnfile1_path, alnfile2_path, phmmerpath, redo):
 
     jointdict = join_two_orgdicts(orgdict1, orgdict2)
 
-    writeout_fasta(outpath, jointdict)
+    writeout_fasta(outpath, jointdict, overwrite=True)
     print(f'Joint alignment written into: {outpath}')
 
     return outpath
