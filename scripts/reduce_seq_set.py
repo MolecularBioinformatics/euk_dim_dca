bf61e6b8cf518d6eb9fb598080678825ee96d938
Yin <hsieh.y.chen@uit.no>
2021-10-20 11:58:10 +0200

    added overwrite messages and changed last element header extraction

--- scripts/reduce_seq_set.py
+++ scripts/reduce_seq_set.py
@@ -45,7 +45,7 @@ def get_orgset_to_delete(headerlist1, headerlist2):
     :param headerlist: list of fastaheaders
     :returns orgset: set of orgtags
     """
-    tags = [header.split()[0].split('_')[1] for header in headerlist1+headerlist2]
+    tags = [header.split()[0].split('_')[-1:] for header in headerlist1+headerlist2]
     return set(tags)
 
 
@@ -62,7 +62,7 @@ def reduce_seq_set(fastafile1, fastafile2, cutoffval):
     Removes sequences that are too long, preserves organism
     agreement between the two fasta files.
 
-    :param fastafile: pathlib.PosixPath
+    :param fastafile: pathlib.PosixPath, path to fasta file
     :param cutoffval: int, maximum sequence length
     """
     seqsdict1, headerlist1 = find_long_seqs(fastafile1, cutoffval)
@@ -73,12 +73,12 @@ def reduce_seq_set(fastafile1, fastafile2, cutoffval):
     if not orgset_toremove:
         raise ValueError(f'No sequences above {cutoffval}. Continuing with original fastas...')
     elif len(orgset_toremove) == len(seqsdict1):
-        raise ValueError(f'All sequences are longer than {cutoffval}. Do you want to remove all seqs?')
+        raise ValueError(f'All sequences are longer than {cutoffval}. Set a more suitable seqlength cutoff. Exiting...') 
 
     seqsdict_reduced1 = del_orgseq_from_dict(seqsdict1, orgset_toremove)
-    print(seqsdict_reduced1)
     seqsdict_reduced2 = del_orgseq_from_dict(seqsdict2, orgset_toremove)
-    print(seqsdict_reduced2)
 
     writeout_fasta(fastafile1, seqsdict_reduced1, overwrite=True)
+    print(f'Sequences reduced in {fastafile1}. Original file overwritten.')
     writeout_fasta(fastafile2, seqsdict_reduced2, overwrite=True)
+    print(f'Sequences reduced in {fastafile2}. Original file overwritten.')
